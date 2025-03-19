6e6bc0e9ee3995714384be57eab6bc5f5db07483
QuangVu04 <quangvu1782004@gmail.com>
2025-03-19 10:46:53 +0700

    lec5

--- Lec3-Hangman/hangMan.cpp
+++ Lec3-Hangman/hangMan.cpp
@@ -2,42 +2,41 @@
 #include <string>
 #include <cstdlib>
 #include <ctime> 
+#include <fstream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-string WORD_LIST[] = {
-    "angle", "ant", "apple", "arch", "arm", "army",
-    "baby", "bag", "ball", "band", "basin", "basket", "bath", "bed", "bee", "bell", "berry",
-    "bird", "blade", "board", "boat", "bone", "book", "boot", "bottle", "box", "boy",
-    "brain", "brake", "branch", "brick", "bridge", "brush", "bucket", "bulb", "button",
-    "cake", "camera", "card", "cart", "carriage", "cat", "chain", "cheese", "chest",
-    "chin", "church", "circle", "clock", "cloud", "coat", "collar", "comb", "cord",
-    "cow", "cup", "curtain", "cushion",
-    "dog", "door", "drain", "drawer", "dress", "drop", "ear", "egg", "engine", "eye",
-    "face", "farm", "feather", "finger", "fish", "flag", "floor", "fly",
-    "foot", "fork", "fowl", "frame", "garden", "girl", "glove", "goat", "gun",
-    "hair", "hammer", "hand", "hat", "head", "heart", "hook", "horn", "horse",
-    "hospital", "house", "island", "jewel", "kettle", "key", "knee", "knife", "knot",
-    "leaf", "leg", "library", "line", "lip", "lock",
-    "map", "match", "monkey", "moon", "mouth", "muscle",
-    "nail", "neck", "needle", "nerve", "net", "nose", "nut",
-    "office", "orange", "oven", "parcel", "pen", "pencil", "picture", "pig", "pin",
-    "pipe", "plane", "plate", "plow", "pocket", "pot", "potato", "prison", "pump",
-    "rail", "rat", "receipt", "ring", "rod", "roof", "root",
-    "sail", "school", "scissors", "screw", "seed", "sheep", "shelf", "ship", "shirt",
-    "shoe", "skin", "skirt", "snake", "sock", "spade", "sponge", "spoon", "spring",
-    "square", "stamp", "star", "station", "stem", "stick", "stocking", "stomach",
-    "store", "street", "sun", "table", "tail", "thread", "throat", "thumb", "ticket",
-    "toe", "tongue", "tooth", "town", "train", "tray", "tree", "trousers", "umbrella",
-    "wall", "watch", "wheel", "whip", "whistle", "window", "wire", "wing", "worm",
-};
+const char DATA_FILE[] ="wordlist.txt";
 
-const int WORD_COUNT = sizeof(WORD_LIST) / sizeof(string);
-
-string chooseWord() {
-    int randomIndex = rand() % WORD_COUNT;
-    return WORD_LIST[randomIndex];
+string toLowerCase(const string& s)
+{
+string res = s;
+int sz = s.size();
+for (int i = 0; i < sz; i++)
+res[i] = tolower(s[i]);
+return res;
 }
+
+string chooseWord(const char* fileName)
+{
+    vector<string> wordList;
+    ifstream file(fileName); //Mở tệp có đường dẫn như trong tham số
+    if (file.is_open()) { // Kiểm tra tệp mở thành công
+    string word;
+    while (file >> word) { //Đọc từng từ đến khi không đọc được nữa
+        wordList.push_back(word);
+    }
+file.close();
+} else cout << "Error opening " << fileName;
+if (wordList.size() > 0) { // nếu có dữ liệu đọc thành công
+    int randomIndex = rand() % wordList.size();
+    return toLowerCase(wordList[randomIndex]);
+
+    } else return ""; 
+}
+
 
 const string FIGURE[] = {
     " ------------- \n"
@@ -111,7 +110,7 @@ string createGuessString(const string& word) {
 int main() {
     srand(time(0));
 
-    string word = chooseWord();
+    string word = chooseWord(DATA_FILE);
     string guess = createGuessString(word);
     char letter;
     int tries = 0;
